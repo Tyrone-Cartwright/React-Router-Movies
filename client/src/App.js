a1a0512c1dbf4587681c7e8d768cac55989d794c
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-26 16:19:04 -0500

    Added Link to MovieList

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
@@ -25,7 +25,7 @@ export default class App extends Component {
         <div>Replace this Div with your Routes</div>
 
         <Route exact path="/" component={MovieList} />
-        <Route path="/movie/:movieId" component={Movie} />
+        <Route path="/movies/:id" component={Movie} />
       </div>
     );
   }
